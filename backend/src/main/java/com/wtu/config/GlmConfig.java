62944ed5eafa971291a2aaaed8ea3fc44a10f87d
coderxguan <3288733739@qq.com>
2025-06-03 09:54:23 +0800

    优化项目结构

--- backend/src/main/java/com/wtu/config/GlmConfig.java
+++ backend/src/main/java/com/wtu/config/GlmConfig.java
@@ -4,8 +4,7 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 /**
  * GLM配置类
